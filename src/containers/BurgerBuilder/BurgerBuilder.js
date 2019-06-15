def06df3d5fa66488d327c453e300565e750452b
shakeelkoper <shakeelkoper@gmail.com>
2019-06-15 21:00:42 +0530

    Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -82,7 +83,29 @@ class BurgerBuilder extends Component{
   }
 
   purchaseContinuehandler = () => {
-    alert('you Continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'shakeel koper',
+        address: {
+          street: 'Test Street 1',
+          zipcode: '54321',
+          country: 'india'
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'fastest',
+    }
+    // alert('you Continue!');
+    axios.post('/orders.json',order)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   }
 
   render(){
