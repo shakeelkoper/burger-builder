973fe9be0ae15899a935d1c1fd04a5cb3dffefde
shakeel koper <shakeel.koper@otiselevator.com>
2020-07-26 13:26:45 +0530

    Building Checkout Container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 class App extends Component {
-  render() {
-    return (
-      <div>
-       <Layout>
-         <BurgerBuilder />
-       </Layout>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Layout>
+					<BurgerBuilder />
+					<Checkout />
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
