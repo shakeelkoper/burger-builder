a2454d77c890d9aa80845b7a0101e85efde8f7e4
shakeel koper <shakeel.koper@otiselevator.com>
2020-08-12 10:45:04 +0530

    finishing reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,16 +26,16 @@ class BurgerBuilder extends Component {
 	};
 
 	componentDidMount() {
-		axios
-			.get(
-				"https://react-my-burger-74d41.firebaseio.com/ingredients.json"
-			)
-			.then((response) => {
-				this.setState({ ingredients: response.data });
-			})
-			.catch((error) => {
-				this.setState({ error: true });
-			});
+		// axios
+		// 	.get(
+		// 		"https://react-my-burger-74d41.firebaseio.com/ingredients.json"
+		// 	)
+		// 	.then((response) => {
+		// 		this.setState({ ingredients: response.data });
+		// 	})
+		// 	.catch((error) => {
+		// 		this.setState({ error: true });
+		// 	});
 	}
 
 	updatePurchaseState() {
