9c390c948e3b16b69b816d89478087d6bf550204
shakeel koper <shakeel.koper@otiselevator.com>
2020-08-29 20:29:22 +0530

    Adding Custom Form Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,6 +15,10 @@ class ContactData extends Component {
 					placeholder: "Your Name",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			street: {
 				elementType: "input",
@@ -23,6 +27,10 @@ class ContactData extends Component {
 					placeholder: "Street",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			zipcode: {
 				elementType: "input",
@@ -31,6 +39,12 @@ class ContactData extends Component {
 					placeholder: "ZIP Code",
 				},
 				value: "",
+				validation: {
+					required: true,
+					minLength: 5,
+					maxLength: 5,
+				},
+				valid: false,
 			},
 			country: {
 				elementType: "input",
@@ -39,6 +53,10 @@ class ContactData extends Component {
 					placeholder: "Country",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			email: {
 				elementType: "input",
@@ -47,6 +65,10 @@ class ContactData extends Component {
 					placeholder: "Your E-Mail",
 				},
 				value: "",
+				validation: {
+					required: true,
+				},
+				valid: false,
 			},
 			deliveryMethod: {
 				elementType: "select",
@@ -88,12 +110,30 @@ class ContactData extends Component {
 				this.setState({ loading: false });
 			});
 	};
+
+	checkValidity(value, rules) {
+		let isValid = false;
+		if (rules.required) {
+			isValid = value.trim() !== "";
+		}
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength;
+		}
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength;
+		}
+	}
+
 	inputChangedHandler = (event, inputIdentifier) => {
 		const updatedOrderForm = {
 			...this.state.orderForm,
 		};
 		const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
 		updatedFormElement.value = event.target.value;
+		updatedFormElement.valid = this.checkValidity(
+			updatedFormElement.value,
+			updatedFormElement.validation
+		);
 		updatedOrderForm[inputIdentifier] = updatedFormElement;
 		this.setState({ orderForm: updatedOrderForm });
 	};
