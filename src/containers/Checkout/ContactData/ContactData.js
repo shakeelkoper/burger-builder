6cc3ec06119d4b6351f3879f6a57fb7f2413cdd5
shakeel koper <shakeel.koper@otiselevator.com>
2020-08-30 07:06:38 +0530

    Improving Visual Feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,6 +19,7 @@ class ContactData extends Component {
 					required: true,
 				},
 				valid: false,
+				touched: false,
 			},
 			street: {
 				elementType: "input",
@@ -31,6 +32,7 @@ class ContactData extends Component {
 					required: true,
 				},
 				valid: false,
+				touched: false,
 			},
 			zipcode: {
 				elementType: "input",
@@ -45,6 +47,7 @@ class ContactData extends Component {
 					maxLength: 5,
 				},
 				valid: false,
+				touched: false,
 			},
 			country: {
 				elementType: "input",
@@ -57,6 +60,7 @@ class ContactData extends Component {
 					required: true,
 				},
 				valid: false,
+				touched: false,
 			},
 			email: {
 				elementType: "input",
@@ -69,6 +73,7 @@ class ContactData extends Component {
 					required: true,
 				},
 				valid: false,
+				touched: false,
 			},
 			deliveryMethod: {
 				elementType: "select",
@@ -135,6 +140,7 @@ class ContactData extends Component {
 			updatedFormElement.value,
 			updatedFormElement.validation
 		);
+		updatedFormElement.touched = true;
 		console.log(updatedFormElement);
 		updatedOrderForm[inputIdentifier] = updatedFormElement;
 		this.setState({ orderForm: updatedOrderForm });
@@ -157,6 +163,7 @@ class ContactData extends Component {
 						value={formElement.config.value}
 						invalid={!formElement.config.valid}
 						shouldValidate={formElement.config.validation}
+						touched={formElement.config.touched}
 						changed={(event) =>
 							this.inputChangedHandler(event, formElement.id)
 						}
