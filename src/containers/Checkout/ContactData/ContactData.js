599a580f9396e0dcb638526c7aa50466d81ba994
shakeel koper <shakeel.koper@otiselevator.com>
2020-08-30 07:24:19 +0530

    Fxing Select Dropdown Error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,6 +83,7 @@ class ContactData extends Component {
 						{ value: "cheapest", displayValue: "Cheapest" },
 					],
 				},
+				validation: {},
 				value: "",
 				valid: true,
 			},
@@ -120,6 +121,9 @@ class ContactData extends Component {
 
 	checkValidity(value, rules) {
 		let isValid = true;
+		if (!rules) {
+			return true;
+		}
 		if (rules.required) {
 			isValid = value.trim() !== "" && isValid;
 		}
