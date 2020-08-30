41ad3640324f450e225ad34e89d5e5b1ced08748
shakeel koper <shakeel.koper@otiselevator.com>
2020-08-30 07:09:31 +0530

    Showing Error Messages

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,6 +3,10 @@ import classes from "./Input.css";
 const input = (props) => {
 	let inputElement = null;
 	const inputClasses = [classes.InputElement];
+	let validationError = null;
+	if (props.invalid && props.touched) {
+		validationError = <p>Please enter a valid value!</p>;
+	}
 
 	if (props.invalid && props.shouldValidate && props.touched) {
 		inputClasses.push(classes.Invalid);
@@ -58,6 +62,7 @@ const input = (props) => {
 		<div className={classes.Input}>
 			<label className={classes.Label}>{props.label}</label>
 			{inputElement}
+			{validationError}
 		</div>
 	);
 };
