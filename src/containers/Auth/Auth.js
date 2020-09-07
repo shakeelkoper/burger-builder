459e266a26b7d0c10b921a6b50fa66884bc5e9f6
shakeel koper <shakeelkoper@gmail.com>
2020-09-07 06:53:20 +0530

    using UpdateObject

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 import classes from "./Auth.css";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions/index";
+import { updateObject } from "../../shared/utility";
 class Auth extends Component {
 	state = {
 		controls: {
@@ -79,18 +80,16 @@ class Auth extends Component {
 	}
 
 	inputChangedHandler = (event, controlName) => {
-		const updatedControls = {
-			...this.state.controls,
-			[controlName]: {
-				...this.state.controls[controlName],
+		const updatedControls = updateObject(this.state.controls, {
+			[controlName]: updateObject(this.state.controls[controlName], {
 				value: event.target.value,
 				valid: this.checkValidity(
 					event.target.value,
 					this.state.controls[controlName].validation
 				),
 				touched: true,
-			},
-		};
+			}),
+		});
 		this.setState({ controls: updatedControls });
 	};
 	submitHandler = (event) => {
