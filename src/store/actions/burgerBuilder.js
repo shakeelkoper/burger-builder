75f03c48695b0d00118c4751a697556d7498be5c
Shakeel <shakeelkoper@gmail.com>
2020-09-19 06:36:23 +0530

    Moving Burger Builder Side Effects into Saga

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,40 +1,31 @@
 import * as actionTypes from "./actionTypes";
 import axios from "../../axios-orders";
 export const addIngredient = (name) => {
-	return {
-		type: actionTypes.ADD_INGREDIENT,
-		ingredientName: name,
-	};
+  return {
+    type: actionTypes.ADD_INGREDIENT,
+    ingredientName: name
+  };
 };
 
 export const removeIngredient = (name) => {
-	return {
-		type: actionTypes.REMOVE_INGREDIENT,
-		ingredientName: name,
-	};
+  return {
+    type: actionTypes.REMOVE_INGREDIENT,
+    ingredientName: name
+  };
 };
 
 export const setIngredients = (ingredients) => {
-	return { type: actionTypes.SET_INGREDIENT, ingredients: ingredients };
+  return { type: actionTypes.SET_INGREDIENT, ingredients: ingredients };
 };
 
 export const fetchIngredientsFailed = () => {
-	return {
-		type: actionTypes.FETCH_INGREDIENTS_FAILED,
-	};
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED
+  };
 };
 
 export const initIngredients = () => {
-	return (dispatch) => {
-		axios
-			.get(
-				"https://react-my-burger-74d41.firebaseio.com/ingredients.json"
-			)
-			.then((response) => {
-				dispatch(setIngredients(response.data));
-			})
-			.catch((error) => {
-				dispatch(fetchIngredientsFailed());
-			});
-	};
+  return {
+    type: actionTypes.INIT_INGREDIENTS
+  };
 };
