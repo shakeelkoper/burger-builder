12c6a01544081b997a702270896b3e943b010392
shakeel <shakeelkoper@gmail.com>
2020-11-08 19:41:55 +0530

    Adjusting Auth and Logout Component

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as actions from "../../../store/actions/index";
 
-class Logout extends Component {
-	componentDidMount() {
-		this.props.onLogout();
-	}
-	render() {
-		return <Redirect to="/" />;
-	}
-}
+const logout = () => {
+	useEffect(() => {
+		props.onLogout();
+	}, []);
+	return <Redirect to="/" />;
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -21,4 +19,4 @@ const mapDispatchToProps = (dispatch) => {
 export default connect(
 	null,
 	mapDispatchToProps
-)(Logout);
+)(logout);
