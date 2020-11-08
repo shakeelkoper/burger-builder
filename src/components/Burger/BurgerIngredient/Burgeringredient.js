cb6a960cc28bb9f803c344b2c1d1cd7dda486bf6
shakeel <shakeelkoper@gmail.com>
2020-11-08 19:52:31 +0530

    using React.memo() & More!

--- src/components/Burger/BurgerIngredient/Burgeringredient.js
+++ src/components/Burger/BurgerIngredient/Burgeringredient.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classes from './BurgerIngredient.css'
-class BurgerIngredient extends Component {
-  render(){
-    let ingredient = null;
-  switch( this.props.type){
-    case('bread-bottom'):
-      ingredient = <div className={classes.BreadBottom}></div>;
-      break;
-    case('bread-top'):
-      ingredient = (
-        <div className={classes.BreadTop}>
-          <div className={classes.Seeds1}></div>
-          <div className={classes.Seeds2}></div>
-        </div>
-      );
-      break;
-    case('meat'):
-      ingredient = <div className={classes.Meat}></div>;
-      break;
-    case('cheese'):
-      ingredient = <div className={classes.Cheese}></div>;
-      break;
-    case('salad'):
-      ingredient = <div className={classes.Salad}></div>;
-      break;
-    case('beef'):
-      ingredient = <div className={classes.Beef}></div>;
-      break;
-    default:
-      ingredient = null;
-  }
-  return ingredient;
-  }
+import React from "react";
+import PropTypes from "prop-types";
+import classes from "./BurgerIngredient.css";
+
+const burgerIngredient = (props) => {
+	let ingredient = null;
+	switch (props.type) {
+		case "bread-bottom":
+			ingredient = <div className={classes.BreadBottom} />;
+			break;
+		case "bread-top":
+			ingredient = (
+				<div className={classes.BreadTop}>
+					<div className={classes.Seeds1} />
+					<div className={classes.Seeds2} />
+				</div>
+			);
+			break;
+		case "meat":
+			ingredient = <div className={classes.Meat} />;
+			break;
+		case "cheese":
+			ingredient = <div className={classes.Cheese} />;
+			break;
+		case "salad":
+			ingredient = <div className={classes.Salad} />;
+			break;
+		case "beef":
+			ingredient = <div className={classes.Beef} />;
+			break;
+		default:
+			ingredient = null;
+	}
+	return ingredient;
 };
 
-BurgerIngredient.propType = {
-  type: PropTypes.string.isRequired
+burgerIngredient.propType = {
+	type: PropTypes.string.isRequired,
 };
 
-export default BurgerIngredient;
+export default burgerIngredient;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
-import classes from './Modal.css';
-import AUX from '../../../hoc/AuxElement/AuxElements';
-import Backdrop from '../Backdrop/Backdrop';
+import React from "react";
+import classes from "./Modal.css";
+import AUX from "../../../hoc/AuxElement/AuxElements";
+import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
-  shouldComponentUpdate(nextprops, nextState){
-    return nextprops.show !== this.props.show || nextprops.children !== this.props.children;
-  }
-  render(){
-    return(
-      <AUX>
-        <div 
-          className={classes.Modal}
-          style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0'
-          }}>
-          {this.props.children}
-        </div>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
-      </AUX>
-    );
-  }
-}
+const modal = (props) => {
+	//   shouldComponentUpdate(nextprops, nextState){
+	//     return nextprops.show !== props.show || nextprops.children !== props.children;
+	//   }
+	return (
+		<AUX>
+			<div
+				className={classes.Modal}
+				style={{
+					transform: props.show
+						? "translateY(0)"
+						: "translateY(-100vh)",
+					opacity: props.show ? "1" : "0",
+				}}
+			>
+				{props.children}
+			</div>
+			<Backdrop show={props.show} clicked={props.modalClosed} />
+		</AUX>
+	);
+};
 
-export default Modal;
+export default React.memo(
+	modal,
+	(prevProps, nextProps) =>
+		nextProps.show == prevProps.show &&
+		nextProps.children === prevProps.children
+);
